5f537ed9a338cd80869a913f22ca15a69f07e305
Miqueas <proyectosmiqueas@gmail.com>
2026-02-13 16:47:56 -0300

    added users.js

---
+++ concepts-game-frontend/src/api/users.js
@@ -0,0 +1,13 @@
+import { api } from './client'
+
+export const usersApi = {
+    getAll: () => api.get('/users'),
+    getById: (id) => api.get(`/users/${id}`),
+    create: ({ username, email, passwordHash }) => 
+        api.post('/users', {
+            username,
+            email,
+            password_hash: passwordHash,
+        }),
+    delete: (id) => api.delete(`/users/${id}`)
+}
