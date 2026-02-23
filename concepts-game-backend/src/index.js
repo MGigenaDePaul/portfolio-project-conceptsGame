ba7d9821bf2142e852922116b2eb63bea220df40
Miqueas <proyectosmiqueas@gmail.com>
2026-02-23 10:56:41 -0300

    changed url of frontend in index.js backend

--- concepts-game-backend/src/index.js
+++ concepts-game-backend/src/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
   origin: [
     "http://localhost:5173",              
-    "https://concepts-game-frontend.onrender.com" 
+    "https://concepts-game.onrender.com" // url frontend
   ],
   credentials: true
 }));
