84f082cb91d17412e155af118ef450d7e188cf4a
Miqueas <proyectosmiqueas@gmail.com>
2026-02-23 05:53:24 -0300

    modified App.jsx routes and fixed linting errors

--- concepts-game-frontend/src/components/ProtectedRoute.jsx
+++ concepts-game-frontend/src/components/ProtectedRoute.jsx
@@ -15,7 +15,7 @@ const Protectedroute = ({ children }) => {
   }
 
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   return children;
